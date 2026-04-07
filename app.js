9f3e0901611a4776cb0c007974b4e412bfc0502c
baazithh <mabasithpullambalavan@gmail.com>
2026-04-07 13:16:22 +0530

    fixed node modules

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require('express');
-const app =express();
+const app = express();
+
 app.use(express.json());
+
 const contactRoutes = require('./routes/contactRoutes');
 const authRoutes = require('./routes/authRoutes');
-const PORT = 3000;
+
+app.use('/api', contactRoutes);
+app.use('/api', authRoutes);
+
+const PORT = 8000;
+app.get('/test', (req, res) => res.send('Server is alive!'));
+app.get('/', (req, res) => res.send("Home page"))
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,11 +1,15 @@
-const model=require('../modules/contactModel');
+const model=require('../models/contactModel');
 const getContacts=(req,res) =>{
+    const contacts = model.getAll();
+    res.json(contacts);
+}
+const addContact = (req, res) => {
     const newContact ={
         id:Date.now(),
         name : req.body.name,
         email : req.body.email
     };
-    model.add(newContact);
+    model.add(addContact);
     res.status(201).json(newContact);
 };
 const deleteContact =(req,res)=>{
@@ -13,4 +17,4 @@ const deleteContact =(req,res)=>{
     model.remove(id);
     res.Send('Deleted')
 };
-module.exports={getContacts,deleteContact,newContact}
+module.exports={getContacts,deleteContact,addContact}
